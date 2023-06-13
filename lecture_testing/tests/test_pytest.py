ed252e6e0086511721786f3433dbebe1fdcc5d71
qasvarych <qasvarych@gmail.com>
2023-06-13 18:54:51 +0300

    testing 2

--- lecture_testing/tests/test_pytest.py
+++ lecture_testing/tests/test_pytest.py
@@ -1,4 +1,5 @@
 import pytest
+import logging
 
 x = 4
 
@@ -38,5 +39,21 @@ def test_clean_1():
     pass
 
 
-def test_clean_2():
-    pass
+class TestFixtures:
+
+    # def test_in_class_1(self, my_first_fixture_placed_in_package):
+    #     print('test_clean_2 does something')
+    #     pass
+    #
+    # def test_in_class_2(self, my_first_fixture_placed_in_package):
+    #     print('test_clean_2 does something')
+    #     pass
+
+    def test_in_class_1(self, class_fixture):
+        assert 'dog' in class_fixture
+        pass
+
+    def test_in_class_2(self, class_fixture):
+        logging.debug('test_clean_2 does something')
+        print('\ntest_clean_2 does something')
+        pass
