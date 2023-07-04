57c5c8e02aa0ce257a0e71fe87bdeab43c709ffb
qasvarych <qasvarych@gmail.com>
2023-07-04 22:37:31 +0300

    checkbox, radio buttons

--- demo/pages/page_check_box.py
+++ demo/pages/page_check_box.py
@@ -28,5 +28,29 @@ class PageCheckBox:
         if 'open' in element.get_attribute('class'):
             self.driver.find_element(By.XPATH, loc).click()
 
+    def check(self, folder_name: str) -> None:
+        label_loc = f'//label[@for="tree-node-{folder_name.lower()}"]'
+        input_ = f'{label_loc}/input[@type="checkbox"]'
+        element_label = self.driver.find_element(By.XPATH, label_loc)
+        element_input = self.driver.find_element(By.XPATH, input_)
+        if not element_input.is_selected():
+            element_label.click()
 
-PageCheckBox('').expand('WorkSpace')
+    def uncheck(self, folder_name: str) -> None:
+        label_loc = f'//label[@for="tree-node-{folder_name.lower()}"]'
+        input_ = f'{label_loc}/input[@type="checkbox"]'
+        element_label = self.driver.find_element(By.XPATH, label_loc)
+        element_input = self.driver.find_element(By.XPATH, input_)
+        if element_input.is_selected():
+            element_label.click()
+
+    def get_results(self) -> list:
+        results_loc = '//div[@id="result"]//span[@class="text-success"]'
+        results_elements = self.driver.find_elements(By.XPATH, results_loc)
+
+        # results: list = []
+        # for element in results_elements:
+        #     results.append(element.text)
+
+        results = [element.text for element in results_elements]
+        return results
